1f435f22d726237ec161fb241b1d23ad5f73355e
Manuela Viola <manuela.viola@gmail.com>
2023-02-14 20:52:12 -0300

    Delete example/ios/Pods directory

--- example/ios/Pods/Target Support Files/Pods-ReactNativeBase-ReactNativeBaseTests/Pods-ReactNativeBase-ReactNativeBaseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeBase_ReactNativeBaseTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeBase_ReactNativeBaseTests
-@end
