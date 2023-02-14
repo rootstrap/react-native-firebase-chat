bd258c56f3a6c5edcc9ec28a3f06fd9d7b621e37
Manuela Viola <manuela.viola@rootstrap.com>
2023-02-14 12:28:42 -0300

    task: start example repo

---
+++ example/ios/Pods/Target Support Files/Pods-ReactNativeBase-ReactNativeBaseTests/Pods-ReactNativeBase-ReactNativeBaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeBase_ReactNativeBaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeBase_ReactNativeBaseTests
+@end
---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Test = () => (
+    <View>
+        <Text>Esto es una prueba</Text>
+        <Text>A ver si funciona!</Text>
+    </View>
+)
+
+export default Test
