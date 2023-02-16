1f0a95a20848c6156630cd881cdf82fda762089d
Manuela Viola <manuela.viola@rootstrap.com>
2023-02-16 18:55:43 -0300

    fix: add colors constants

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,9 +1,15 @@
 import { StyleSheet } from "react-native";
+import {
+  BLUE,
+  METALIC_BLUE,
+  SKY_BLUE,
+  SKY_BLUE_LIGHT,
+} from "../constants/colors";
 
 const styles = StyleSheet.create({
   generalChatContainer: {
     flex: 1,
-    backgroundColor: "#caf8fc",
+    backgroundColor: SKY_BLUE_LIGHT,
     paddingBottom: 80,
     width: "100%",
   },
@@ -19,7 +25,7 @@ const styles = StyleSheet.create({
   }),
 
   messageContainer: ({ addMargin, isMyComment, isSameUser }) => ({
-    backgroundColor: isMyComment ? "#4b849c" : "skyblue",
+    backgroundColor: isMyComment ? METALIC_BLUE : SKY_BLUE,
     borderRadius: 10,
     marginHorizontal: addMargin ? 40 : 10,
     marginTop: isSameUser ? 5 : 20,
@@ -45,7 +51,7 @@ const styles = StyleSheet.create({
   },
 
   sendMessageContainer: {
-    backgroundColor: "skyblue",
+    backgroundColor: SKY_BLUE,
     flexDirection: "row",
     justifyContent: "space-between",
     padding: 20,
@@ -56,23 +62,22 @@ const styles = StyleSheet.create({
   },
 
   messageInput: {
-    borderColor: "blue",
+    backgroundColor: SKY_BLUE_LIGHT,
     borderRadius: 10,
-    borderWidth: 2,
     flex: 1,
     marginHorizontal: 10,
     paddingHorizontal: 10,
   },
 
   iconContainer: ({ pressed }) => ({
-    backgroundColor: pressed ? "#485a5c" : "transparent",
+    backgroundColor: pressed ? METALIC_BLUE : "transparent",
     borderRadius: 25,
     marginRight: 10,
     padding: 10,
   }),
 
   sendMessageIcon: ({ disabled }) => ({
-    tintColor: disabled ? "#485a5c" : "blue",
+    tintColor: disabled ? METALIC_BLUE : BLUE,
     height: 30,
     width: 30,
   }),
